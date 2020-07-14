5e4d633230ae9afa19c9680fdcac9a9a8d9ab80b
wuyudi <wuyudi1109@gmail.com>
2020-07-15 03:03:14 +0800

    rkt 多个返回值有点麻烦，最好返回单个

---
+++ 1.3.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+
+(define( sum-of-2-sq a b)
+  (+(*  a a)
+    (*  b b))
+    )
+(define (2-large a b c)
+  (cond ((and (>= a b) (>= b c))   ( sum-of-2-sq a b))
+        ((and (>= b a) (>= c a))   ( sum-of-2-sq c b))
+        ((and (>= c b) (>= a b))   ( sum-of-2-sq a c))
+        )
+  )
+(2-large 1 2 3)
